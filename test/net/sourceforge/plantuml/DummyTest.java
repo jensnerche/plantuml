3d7bdafe7e425592f21007a716088155a89b212c
jn <j.nerche@kontext-e.de>
2015-09-21 13:31:04 +0200

    added a dummy test and jQA

---
+++ test/net/sourceforge/plantuml/DummyTest.java
@@ -0,0 +1,13 @@
+package net.sourceforge.plantuml;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void thatShouldFail() throws Exception {
+        Assert.assertTrue(1 < 3);
+    }
+
+}
