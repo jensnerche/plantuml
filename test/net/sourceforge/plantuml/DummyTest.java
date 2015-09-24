2bfb90bdae73020eb036f58470011684792f1898
jn <j.nerche@kontext-e.de>
2015-09-24 09:18:07 +0200

    removed empty lines

--- test/net/sourceforge/plantuml/DummyTest.java
+++ test/net/sourceforge/plantuml/DummyTest.java
@@ -4,10 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DummyTest {
-
     @Test
     public void thatShouldFail() throws Exception {
         Assert.assertTrue(1 < 3);
     }
-
 }
